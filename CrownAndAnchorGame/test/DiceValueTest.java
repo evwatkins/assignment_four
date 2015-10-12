f1d1e7a66d58bed92f6b1bf20e8e32b27bb57c3e
Evan Watkins <evwatkins@gmail.com>
2015-10-12 22:08:42 +1100

    Created new java project as source files not setup as java project

---
+++ CrownAndAnchorGame/test/DiceValueTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class DiceValueTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetRandom() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
