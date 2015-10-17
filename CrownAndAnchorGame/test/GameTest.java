513e3ebc79177bbe10b84ef88ce52c2163ca53b8
Evan Watkins <evwatkins@gmail.com>
2015-10-17 12:13:19 +1100

    Files added to Project

---
+++ CrownAndAnchorGame/test/GameTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class GameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGame() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetDiceValues() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPlayRound() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ CrownAndAnchorGame/test/PlayerTest.java
@@ -0,0 +1,68 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class PlayerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetLimit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetLimit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testBalanceExceedsLimit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testBalanceExceedsLimitBy() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testTakeBet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testReceiveWinnings() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
